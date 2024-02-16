915c578be87e638daa69e96c688ae8ddd587c343
Manish Sharma <manish.sharma@volansys.com>
2024-02-16 12:45:53 +0530

    AppOne added

---
+++ AppOne/AppOne/AppOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOneApp.swift
+//  AppOne
+//
+//  Created by Manish Sharma on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
