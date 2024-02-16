a92b07d6e9f220e0a640b66edfc4def362ee7594
Manish Sharma <manish.sharma@volansys.com>
2024-02-16 12:50:34 +0530

    appTwo_added

---
+++ AppTwo/AppTwo/AppTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTwoApp.swift
+//  AppTwo
+//
+//  Created by Manish Sharma on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
